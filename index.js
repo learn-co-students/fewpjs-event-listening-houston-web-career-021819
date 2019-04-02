67dbe88c272632d77857427c756c5f06c604ce28
JingChen <jingchenjc2019@gmail.com>
2019-04-02 01:46:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+  //get input from the html page
+  const input = document.getElementById('input')
+  //use .addEventListener to set up event for that input;
+  input.addEventListener('click', function(event){alert('I was clicked!');});
 }
