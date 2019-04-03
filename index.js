30ca6b96a699ebefe1a2c1850da6e632c51a03a3
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-04-03 13:17:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input');
 function addingEventListener() {
+        
+        input.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('I was clicked!');
+        });
 }
+addingEventListener();
